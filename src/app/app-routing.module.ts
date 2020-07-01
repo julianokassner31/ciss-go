815386f201b86bb70a6756e9e94b81a3d2b422d0
jkassner <julianokassner@hotmail.com>
2020-07-01 20:27:30 -0300

    Funcionar lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { LoginComponent } from './pages/login/login.component';
 
@@ -22,7 +22,6 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      preloadingStrategy: PreloadAllModules,
       useHash: true,
     }),
   ],
--- src/app/pages/carrinho/carrinho.module.ts
+++ src/app/pages/carrinho/carrinho.module.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { HeaderComponent } from '../../components/header/header.component';
 import { CarrinhoRoutingModule } from './carrinho-routing.module';
 import { CarrinhoComponent } from './carrinho.component';
 
-NgModule({
+@NgModule({
   imports: [CommonModule, CarrinhoRoutingModule],
-  declarations: [CarrinhoComponent, HeaderComponent],
+  declarations: [CarrinhoComponent],
   providers: [],
   exports: [],
-});
+})
 export class CarrinhoModule {}
